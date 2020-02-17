d1db1714e1f6bc6159ede8d7f002bf221245ec25
Satyam Arora <brogrammer2000@users.noreply.github.com>
2020-02-17 16:07:07 +0200

    added comments php page

--- blue_pancakes.php
+++ blue_pancakes.php
@@ -128,68 +128,7 @@
         </div>
 
 
-        <div class="row" id="comments">
-            <div class="col-lg-5" id="comments_input">
-                <h2>Leave a Reply</h2>
-                <hr>
-                <p>Your email address will not be published. Required fields are marked <span style="color: red">*</span></p>
-
-                <form>
-
-
-                    <p>NAME <span style="color: red">*</span></p>
-                    <input id="input_box" class="info" type="text" name="name">
-
-                    <p>EMAIL <span style="color: red">*</span></p>
-                    <input id="input_box" class="info" type="email" name="email">
-
-                    <p>COMMENT</p>
-                    <textarea id="input_box" class="info" style="height: 150px;" name="comment"></textarea>
-
-                    <p>RATING</p>
-                    <input id="input_box" class="info" type="text" name="website">
-                    <br>
-                    <br>
-
-
-                    <button style="background-color: red" type="button" class="btn btn-primary button">POST COMMENT</button>
-                    <hr>
-
-                </form>
-            </div>
-
-
-
-
-            <div class="col-lg-7 comment" id="comments">
-                <h2>Comments</h2>
-                <hr>
-                <div id="input_box" class="box">
-                    <h4>SCARLETT JOHANSONN</h4>
-                    <p>Try grandma’s spot remover to get stains out. It is pretty awesome stuff</p>
-
-                </div>
-
-                <div id="input_box" class="box">
-                    <h4>ANGELINA JOLIE</h4>
-                    <p>MY mOMMY TAUGHT ME THAT IF YOU REALLY WANT FLUFFY PANCAKES, SEPARATE THE EGSS, AND BEST THE
-                        WHITES TIL SOFT PEAKS FORM AND FOLD THEM IN. iT’S A NEVER FAIL FOR FLUFFY hOTCAKES, AS SHE
-                        CALLED THEM.</p>
-
-                </div>
-
-                <div id="input_box" class="box">
-                    <h4>AARUSHI BHATNAGAR</h4>
-                    <p>Greetings, I simply made these pancakes for supper. They looked so great I couldn’t support
-                        myself. I follwed the formula careful and they turned out flawless. I utilized solidified
-                        blueberries, I didn’t blend them into the hitter yet I put the blueberries straightforwardly
-                        into the scoop consequently my player did not turn blue.</p>
-
-                </div>
-                <hr>
-            </div>
-
-        </div>
+       
 
 
     </div>
