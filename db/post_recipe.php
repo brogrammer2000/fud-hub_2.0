f8e43e758c56fe9f2c088d841cfed0e8fa960145
siddharth406 <60779906+siddharth406@users.noreply.github.com>
2020-03-04 00:47:03 -0800

    Update post_recipe.php

--- db/post_recipe.php
+++ db/post_recipe.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "admin";
+$password = "admin123";
 $dbname = "post";
 // creating connection
 $conn = new mysqli ($servername, $username, $password, $dbname);
